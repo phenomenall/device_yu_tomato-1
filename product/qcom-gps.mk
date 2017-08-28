0e864ef7ca7c92ba16ab8918c8a2a966f343ad03
LuK1337 <priv.luk@gmail.com>
2017-08-28 11:59:53 +0200

    tomato: gps: Add flp shim
    Change-Id: I4cf64915561c60b24a1ab85f65f73b012385ea56

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -9,7 +9,8 @@ PRODUCT_PACKAGES += \
 
 # GPS Shim
 PRODUCT_PACKAGES += \
-    libshims_get_process_name
+    libshims_get_process_name \
+    libshims_flp
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
