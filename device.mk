02b77ad1c897710d3999fe895e5e375174d52db1
Shekhawat2 <anandsingh215@yahoo.com>
2018-01-22 17:28:25 +0530

    Include Gapps From PixelExperience vendor/gapps/config.mk

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ $(call inherit-product, device/cyanogen/msm8916-common/msm8916.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Gapps
+$(call inherit-product, vendor/gapps/config.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
