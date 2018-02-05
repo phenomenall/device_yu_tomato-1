d8b1a0ea6afc4491a7705a34f5548b2be8b5eb23
Shekhawat2 <anandsingh215@yahoo.com>
2018-02-05 18:33:26 +0530

    Fux a typo.

--- board/qcom-sdclang.mk
+++ board/qcom-sdclang.mk
@@ -2,6 +2,6 @@
 ifneq ($(HOST_OS),darwin)
 SDCLANG := true
 SDCLANG_PATH := prebuilts/clang/host/linux-x86/clang-4053586/bin
-SDCLANG_LTO_DEFS := device/qcom/common/dllvm-lto-defs.mk
+SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
 endif
 
