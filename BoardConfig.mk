64b0231057f9b0128eaf96439d86378e8c7ca80d
Cruzer1 <iamcruzer9090@gmail.com>
2018-07-08 06:51:46 +0000

    tomato:Remove fragments back to old conf. actually old conf is best not bulky Doze derp fix
    signed-off-by :Cruzer1<iamcruzer9090@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2018 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,22 +14,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#include common tree stuff
 include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
-include device/yu/tomato/board/*.mk
+DEVICE_PATH := device/yu/tomato
+
 
 # Assertions
-TARGET_BOARD_INFO_FILE := device/yu/tomato/board-info.txt
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Inherit from proprietary files
 include vendor/yu/tomato/BoardConfigVendor.mk
 
+#Audio
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+
+#Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+#Camera
+BOARD_CAMERA_SENSORS := imx135_cp8675 imx214_cp8675 ov5648_cp8675
+TARGET_USE_VENDOR_CAMERA_EXT := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+#Dexpreopt
+WITH_DEXPREOPT := false
+DONT_DEXPREOPT_PREBUILTS := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+
+#Filesystem
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 10485760
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13576175616 # 13576192000 - 16384
+# Use mke2fs instead of make_ext4fs
+TARGET_USES_MKE2FS := true
+
+#Gps
+TARGET_NO_RPC := true
+USE_DEVICE_SPECIFIC_GPS := true
+
+# Init
+TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_tomato.cpp
+
+# Kernel
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_SEPARATED_DT := true
+LZMA_RAMDISK_TARGETS := recovery
+TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+#Libshims
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib64/libflp.so|libshims_flp.so \
+    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/libflp.so|libshims_flp.so \
+    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
+
+#Lights
+BOARD_LIGHTS_VARIANT := aw2013
+TARGET_PROVIDES_LIBLIGHT := true
+
+# LineageHW
+BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
+
+#Media
+TARGET_USES_MEDIA_EXTENSIONS := true
+
+# Power
+TARGET_POWER_SET_FEATURE_LIB := libpower_set_feature_tomato
+
+#HALS Varriant
 TARGET_QCOM_DISPLAY_VARIANT := caf-msm8916
 TARGET_QCOM_AUDIO_VARIANT := caf-msm8916
 TARGET_QCOM_MEDIA_VARIANT := caf-msm8916
 
 # Properties
-TARGET_SYSTEM_PROP += device/yu/tomato/system.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 #Boot Animation
 TARGET_BOOTANIMATION_HALF_RES := true
@@ -36,5 +102,28 @@ TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
-#Audio
-AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+# Recovery
+TARGET_RECOVERY_DENSITY := xhdpi
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
+
+# SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+#TWRP
+ifeq ($(WITH_TWRP),true)
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)/twrp
+TW_INCLUDE_CRYPTO := true
+TW_THEME := portrait_hdpi
+endif
+
+# Widevine
+BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 3
+
+#WIFI
+# Wifi CONFIG_EAP_PROXY := qmi
+#CONFIG_EAP_PROXY_DUAL_SIM := true
+TARGET_PROVIDES_WCNSS_QMI := true
+# The uncompressed arm64 is too large, split wifi for now
+#WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+#WIFI_DRIVER_MODULE_NAME := "wlan"
--- device.mk
+++ device.mk
@@ -21,6 +21,40 @@ $(call inherit-product, device/cyanogen/msm8916-common/msm8916.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/acdb/MTP_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/MTP_General_cal.acdb:system/etc/acdbdata/MTP/MTP_General_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/MTP_Global_cal.acdb:system/etc/acdbdata/MTP/MTP_Global_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/MTP_Handset_cal.acdb:system/etc/acdbdata/MTP/MTP_Handset_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/MTP_Hdmi_cal.acdb:system/etc/acdbdata/MTP/MTP_Hdmi_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/MTP_Headset_cal.acdb:system/etc/acdbdata/MTP/MTP_Headset_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/MTP_Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    yl_btmac
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    camera.device@1.0-impl \
+    camera.msm8916 \
+    libmm-qcamera \
+    Snap
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false \
+    persist.camera.hal.debug.mask=0
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
@@ -35,12 +69,177 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
-# Include package config fragments
-include $(LOCAL_PATH)/product/*.mk
+#Dalvik
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
+# Doze mode
+ PRODUCT_PACKAGES += \
+     YUDoze
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916 \
+    libgnsspps
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
+# GPS Shim
+PRODUCT_PACKAGES += \
+    libshims_get_process_name \
+    libshims_flp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
+# IMS properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enabled_by_hw=1
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.qti.ims.sh \
+    fstab.qcom \
+    init.target.rc \
+    set_baseband.sh
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/goodix.kl:system/usr/keylayout/goodix.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8916
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
+#Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+# Widevine DRM symbol, boringssl-compat subset
+PRODUCT_PACKAGES += \
+    libshim_parcel \
+    libshim_boringssl
+# OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libOmxSwVencHevc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.audio.sink=280 \
+    vidc.dec.downscalar_width=1920 \
+    vidc.dec.downscalar_height=1088 \
+    vidc.disable.split.mode=1 \
+    vidc.enc.disable_bframes=1 \
+    vidc.enc.disable_pframes=1 \
+    vidc.enc.narrow.searchrange=1
+
+#RADIO
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    persist.data.target=dpm1 \
+    persist.radio.multisim.config=dsds \
+    ro.config.always_show_roaming=true \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+    ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+    ro.telephony.default_network=9,9 \
+    ril.subscription.types=NV,RUIM
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+# Telephony packages
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml
+
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    libjni_proximityCalibrate \
+    ProximityCalibrate \
+    sensors.msm8916
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+
+# USB ID
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=F003 \
+    ro.usb.id.mtp_adb=9039 \
+    ro.usb.id.ptp=904D \
+    ro.usb.id.ptp_adb=904E \
+    ro.usb.id.ums=F000 \
+    ro.usb.id.ums_adb=9015 \
+    ro.usb.vid=05c6
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libwifi-hal-qcom \
+    libwcnss_qmi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+
+# Permissions
+PRODUCT_COPY_FILES += \
+             frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+             frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+             frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+             frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+             frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+             frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+             frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+             frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+             frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+             frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+             $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+
 
 # Call the proprietary setup
 $(call inherit-product, vendor/yu/tomato/tomato-vendor.mk)
 
+#Customapps
 PRODUCT_PACKAGES +=\
 MiXplorer
 
