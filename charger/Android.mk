814477bbf6396ce19550e6f4346254f4c95976cf
phenomenall <venkysaihot@gmail.com>
2018-08-05 07:26:45 +0000

    Charger : Changes
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- charger/Android.mk
+++ charger/Android.mk
@@ -52,7 +52,7 @@ include $(CLEAR_VARS)
 
 define _add-charger-image
 include $$(CLEAR_VARS)
-LOCAL_MODULE := vendor_cm_charger_$(notdir $(1))
+LOCAL_MODULE := device_yu_tomato_cm_charger_$(notdir $(1))
 LOCAL_MODULE_STEM := $(notdir $(1))
 _img_modules += $$(LOCAL_MODULE)
 LOCAL_SRC_FILES := $1
