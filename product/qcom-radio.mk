15eb5c05fa03119f9e13afd7f810fcf9fc4357d5
phenomenall <venkysaihot@gmail.com>
2018-10-03 07:29:08 +0000

    Revert "Add some indian APN profiles"
    This reverts commit 873ae899576fda10f5e440d0aa12e948d4e8601d.

---
+++ product/qcom-radio.mk
@@ -0,0 +1,26 @@
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    persist.data.target=dpm1 \
+    persist.radio.multisim.config=dsds \
+    ro.config.always_show_roaming=true \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+    ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+    ro.telephony.default_network=9,9 \
+    ril.subscription.types=NV,RUIM
+
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
