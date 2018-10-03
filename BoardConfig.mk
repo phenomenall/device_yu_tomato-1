8b191c95aa101e5591a8e213d9899886e926566e
phenomenall <venkysaihot@gmail.com>
2018-10-03 05:36:19 +0000

    kernel:linaro 7.3.1 toolchain
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 BOARD_CAMERA_SENSORS := imx135_cp8675 imx214_cp8675 ov5648_cp8675
 TARGET_USE_VENDOR_CAMERA_EXT := true
 USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE := true
 
 #Dexpreopt
 WITH_DEXPREOPT := false
@@ -68,6 +69,8 @@ BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-opt-linux-android/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-opt-linux-android-
 
 #Libshims
 TARGET_LD_SHIM_LIBS := \
