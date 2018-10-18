e38eed0ebbdb0c5adab280e1aadd5e97606feb53
phenomenall <venkysaihot@gmail.com>
2018-10-18 14:54:11 +0000

    bt: fix bt (try-1)

--- device.mk
+++ device.mk
@@ -47,7 +47,12 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    yl_btmac
+    yl_btmac \
+    libbt-vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
