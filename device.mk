0bbfbd12b836f4b6577fbd906ceb30a789160f53
phenomenall <venkysaihot@gmail.com>
2018-10-21 10:33:32 +0000

    Revert "bt: fix bt (try-1)"
    This reverts commit e38eed0ebbdb0c5adab280e1aadd5e97606feb53.

--- device.mk
+++ device.mk
@@ -47,12 +47,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    yl_btmac \
-    libbt-vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    yl_btmac
 
 # Camera
 PRODUCT_PACKAGES += \
